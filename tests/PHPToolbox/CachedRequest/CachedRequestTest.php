7f30c94aee5e0f015ee647b3c29f1a6fe00596c3
Codemis <johnathan@jpulos.com>
2015-06-05 12:54:11 -0700

    Setup the new composer autoloader on tests.

--- tests/PHPToolbox/CachedRequest/CachedRequestTest.php
+++ tests/PHPToolbox/CachedRequest/CachedRequestTest.php
@@ -1,26 +1,28 @@
 <?php
 /**
  * This file is part of  PHPToolbox, a library of useful PHP code.
- * 
+ *
  * PHPToolbox is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * PHPToolbox is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see 
- * <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see
+ *  <http://www.gnu.org/licenses/>.
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * 
+ *
  */
-namespace PHPToolbox\CachedRequest;
+namespace PHPToolbox;
+
+use \PHPToolbox\CachedRequest\CachedRequest;
 
 /**
  * A test for the CachedRequest class
@@ -248,7 +250,7 @@ class CachedRequestTest extends \PHPUnit_Framework_TestCase
     }
     /**
      * Tests that writeCacheFile() writes content to a cache file
-     * 
+     *
      * @covers CachedResult::writeCacheFile
      * @access public
      * @author Johnathan Pulos
